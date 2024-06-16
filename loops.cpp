3e97acfa18db4bf7c36ddc0a779db33a9bd9a509
saksham <saksham>
2024-06-16 17:31:59 +0530

    loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int i = 0;
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // while (i<n){
+    //     int j = 0;
+    //     while (j<n){
+    //         cout<<"*";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // int i=1;
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // while (i<=n){
+    //     int j=0;
+    //     while (j<n){
+    //         cout<<i;
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // int i=1;
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // int k=1;
+    // while (i<=n){
+    //     int j=1;
+    //     while (j<=n){
+    //         cout<<k;
+    //         j++;
+    //         k++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // int i=1;
+    // int n;
+    // cout<<"Enter number: ";
+    // cin>>n;
+    // while (i<=n){
+    //     int j=1;
+    //     while (j<=i){
+    //         cout<<"*";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    int i=1;
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+    while (i<=n){
+        int j=1;
+        while (j<=i){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
